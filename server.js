50aefcb2d80e1f2e3f550f7300fd9a2c5c539d97
felix1805 <felix1805@gmail.com>
2023-10-20 16:12:08 -0400

    added .get for quotescollection to render as array in console, added ejs view engine.

--- server.js
+++ server.js
@@ -2,18 +2,24 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
-const connectionString = ''
+const connectionString = 'INSERT CONNECTION STRING HERE'
 
-MongoClient.connect(connectionString, { useUnifiedTopology: true })
+MongoClient.connect(connectionString)
   .then(client => {
     console.log('Connected to Database')
 
     const db = client.db('star-wars-quotes')
     const quotesCollection = db.collection('quotes')
 
+    app.set('view engine', 'ejs')
     app.use(bodyParser.urlencoded({ extended: true }))
 
     app.get('/', (req, res) => {
+      quotesCollection.find().toArray()
+        .then(results => {
+          console.log(results)
+        })
+        .catch(error => console.error(error))
       res.sendFile(__dirname + '/index.html')
     })
     app.post('/quotes', (req, res) => {
